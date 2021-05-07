bbbf598d26da141c68a2258f5e08a5581023b5f7
myshched <mykyta.shchedrolosiev@gmail.com>
2021-05-07 15:39:42 +0200

    Added script to create ParticleFiles

---
+++ script/CreatePartFiles.py
@@ -0,0 +1,44 @@
+import os
+from itertools import product
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+directory_out = root_dir+'/../particle_files'
+
+try:
+    os.stat(directory_out)
+except:
+    os.mkdir(directory_out)
+
+baseParticleFile="""
+BLOCK MASS
+#  PDG code   mass   particle
+   1000015    %MSTAU%  # ~tau_1
+   1000022    %MLSP%  # ~chi_10
+Block
+# DECAY TABLE
+DECAY    1000015  %CTAU% # tau + ~chi_10
+#          BR          NDA       ID1     ID2
+           1.0         2          15    1000022
+"""
+
+hBarCinGeVmm = 1.973269788e-13
+
+mstau_list = [90, 100, 150, 200]
+mlsp_list  = [1, 90]
+ctau0_list  = [100, 500, 1000]
+
+grid = list(product(*[mstau_list, mlsp_list, ctau0_list]))
+print "create:",len(grid),"files"
+
+for mstau, mlsp, ctau0 in grid:
+    print "mstau:",mstau,"mlsp:",mlsp,"ctau:",ctau0
+    ctau = hBarCinGeVmm/ctau0
+    ParticleFile = baseParticleFile.replace('%MSTAU%','%e' % mstau)
+    ParticleFile = ParticleFile.replace('%MLSP%','%e' % mlsp)
+    ParticleFile = ParticleFile.replace('%CTAU%','%e' % ctau)
+    f = open(directory_out + ("/particles_stau_mstau{}GeV_mlsp{}GeV_ctau{}mm.txt"
+        .format(mstau,mlsp, ctau0)), "w")
+    f.write(ParticleFile)
+    f.close()
+
+print "done"

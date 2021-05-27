587648392d3c73ef0b345a8e87e977e0305e8b76
myshched <mykyta.shchedrolosiev@gmail.com>
2021-05-27 18:55:49 +0200

    Updated cmsDriver command with Exotica_HSCP_SIM and genPlusSimParticles modules, also split the production into two steps

--- script/CreateDrivers.sh
+++ script/CreateDrivers.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-ENV_PATH=/afs/cern.ch/user/m/myshched/STauGENProduction
+# ENV_PATH=/afs/cern.ch/user/m/myshched/STauGENProduction
 
 ABS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTDIR=${ABS_PATH}/..
+
+ENV_PATH=${ABS_PATH}/../envs
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export CMSSW_GIT_REFERENCE=/cvmfs/cms.cern.ch/cmssw.git.daily
@@ -22,7 +24,7 @@ fi
 echo "Creating cmsDrive for RAWSIM"
 #cmsDriver.py Configuration/GenProduction/python/SUS-RunIIFall18GS-00022-fragment.py --python_filename ${OUTDIR}/python/SUS-RunIIFall18GS-00022_1_cfg.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:SUS-RunIIFall18GS-00022.root --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --customise_commands "process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(200)"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(191)" --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --mc || exit $? ;
 
-cmsDriver.py Configuration/GenProduction/python/SUS-RunIIFall18GS-00022-fragment.py --python_filename ${OUTDIR}/python/SUS-RunIIFall18GS-00022_1_cfg.py --eventcontent RAWSIM --outputCommand 'keep *_genParticlePlusGeant_*_*' --customise SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeProduce,SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeKeep,SimG4Core/CustomPhysics/Exotica_HSCP_SIM_cfi,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:SUS-RunIIFall18GS-00022.root --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --customise_commands "process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(200)"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(191)" --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --mc || exit $? ;
+cmsDriver.py Configuration/GenProduction/python/SUS-RunIIFall18GS-00022-fragment.py --python_filename ${OUTDIR}/python/SUS-RunIIFall18GS_updated_step1.py --eventcontent RAWSIM --outputCommand 'keep *_genParticlePlusGeant_*_*' --customise SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeProduce,SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeKeep,SimG4Core/CustomPhysics/Exotica_HSCP_SIM_cfi,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:SUS-RunIIFall18GS-00022.root --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --customise_commands "process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(200)"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(191)" --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --mc -n 100 || exit $? ;
 
 eval `scram runtime -sh`
 
@@ -36,13 +38,16 @@ else
   exit 1
 fi
 
-echo "Creating cmsDrive for PREMIXRAW"
-cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18DRPremix-00225_1_cfg.py --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --fileout file:SUS-RunIIAutumn18DRPremix-00225_0.root --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW" --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --geometry DB:Extended --filein file:SUS-RunIIFall18GS-00022.root --datamix PreMix --era Run2_2018 --no_exec --mc || exit $? ;
+# echo "Creating cmsDrive for PREMIXRAW"
+# cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18DRPremix-00225_1_cfg.py --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --fileout file:SUS-RunIIAutumn18DRPremix-00225_0.root --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW" --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --geometry DB:Extended --filein file:SUS-RunIIFall18GS-00022.root --datamix PreMix --era Run2_2018 --no_exec --mc || exit $? ;
 
-echo "Creating cmsDrive for AODSIM"
-cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18DRPremix-00225_2_cfg.py --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --fileout file:SUS-RunIIAutumn18DRPremix-00225.root --conditions 102X_upgrade2018_realistic_v15 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --filein file:SUS-RunIIAutumn18DRPremix-00225_0.root --era Run2_2018 --runUnscheduled --no_exec --mc || exit $? ;
+# echo "Creating cmsDrive for AODSIM"
+# cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18DRPremix-00225_2_cfg.py --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --fileout file:SUS-RunIIAutumn18DRPremix-00225.root --conditions 102X_upgrade2018_realistic_v15 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --filein file:SUS-RunIIAutumn18DRPremix-00225_0.root --era Run2_2018 --runUnscheduled --no_exec --mc || exit $? ;
 
-echo "Creating cmsDrive for MINIAODSIM"
-cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18MiniAOD-00351_1_cfg.py --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --fileout file:SUS-RunIIAutumn18MiniAOD-00351.root --conditions 102X_upgrade2018_realistic_v15 --step PAT --geometry DB:Extended --filein file:SUS-RunIIAutumn18DRPremix-00225.root --era Run2_2018 --runUnscheduled --no_exec --mc || exit $? ;
+# echo "Creating cmsDrive for MINIAODSIM"
+# cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIAutumn18MiniAOD-00351_1_cfg.py --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --fileout file:SUS-RunIIAutumn18MiniAOD-00351.root --conditions 102X_upgrade2018_realistic_v15 --step PAT --geometry DB:Extended --filein file:SUS-RunIIAutumn18DRPremix-00225.root --era Run2_2018 --runUnscheduled --no_exec --mc || exit $? ;
 
-eval `scram runtime -sh`
+# eval `scram runtime -sh`
+
+# Combine 3 steps into one
+cmsDriver.py  --python_filename ${OUTDIR}/python/SUS-RunIIFall18GS_updated_step2.py --eventcontent MINIAODSIM --outputCommand 'keep *_genParticlePlusGeant_*_*' --customise SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeProduce,SimG4Core/CustomPhysics/GenPlusSimParticles_cfi.customizeKeep,Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --fileout file:SUS-RunIIAutumn18MiniAOD-00351.root --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW" --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018,RAW2DIGI,L1Reco,RECO,RECOSIM,EI,PAT --procModifiers premix_stage2 --geometry DB:Extended --filein file:SUS-RunIIFall18GS-00022.root --datamix PreMix --era Run2_2018 --runUnscheduled --no_exec --mc || exit $? ;

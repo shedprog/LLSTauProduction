1bbbe9ac2242cddf3b05133a3ff1c8ff02fc38d0
myshched <mykyta.shchedrolosiev@gmail.com>
2022-05-16 14:45:04 +0200

    Added 1000mm step5 setup

---
+++ crab/submit_stau400_mlsp1_ctau1000mm/crab_cfg_step_5.py
@@ -0,0 +1,31 @@
+# TEMPLATE used for automatic script submission of multiple datasets
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'SUS-RunIISummer20UL18MiniAOD-stau400_lsp1_ctau1000mm_v4'
+config.General.workArea = 'crab_projects'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../../python/SUS-RunIISummer20UL18MiniAODv2_cfg.py'
+
+config.JobType.maxJobRuntimeMin = 5*60
+config.JobType.maxMemoryMB = 2500
+
+config.section_("Data")
+
+config.Data.inputDataset = '/SUS-RunIISummer20UL18GEN-stau400_lsp1_ctau1000mm_v4/myshched-RECO-b48a278ad9b80207fe073529bf32d66e/USER'
+config.Data.inputDBS = 'phys03'
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+
+config.Data.outLFNDirBase = '/store/user/myshched/mc/UL2018-pythia-v4'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'MiniAOD'
+
+config.section_("Site")
+config.Site.whitelist = ['T2_DE_DESY']
+config.Site.storageSite = 'T2_DE_DESY'
